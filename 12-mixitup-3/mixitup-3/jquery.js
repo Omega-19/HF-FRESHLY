8f71bd93410f1c1c279c2727105908978ed9d2f0
Omega-19 <nicolevigan1@gmail.com>
2023-06-02 12:58:58 +0200

    yes

---
+++ 12-mixitup-3/mixitup-3/jquery.js
@@ -0,0 +1,18 @@
+body{
+
+}
+.item{
+    display:inline-CSSLayerBlockRule;
+    border: 1px solid lightgray;
+    width:100px;
+    height:100px;
+    box-shadow:0 5px 5px rgba(0, 0, 0, .3)
+    margin
+}
+.red::before{
+    content:"";
+    display:CSSLayerBlockRule;
+    background-color: red;
+    padding: 5rem;
+
+}
